ff7b3beb365140558b3898d12ac00e538b3b54e5
kyleb13 <38119558+kyleb13@users.noreply.github.com>
2018-10-24 19:07:29 -0700

    initial

---
+++ app/src/main/java/group1/tcss450/uw/edu/messageappgroup1/MainActivity.java
@@ -0,0 +1,13 @@
+package group1.tcss450.uw.edu.messageappgroup1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
