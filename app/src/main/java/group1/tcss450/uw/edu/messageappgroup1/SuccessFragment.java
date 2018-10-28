e69e7ede600a69f3ad74d800339af9f90b8c7609
Sam B <sam2b@uw.edu>
2018-10-27 21:04:54 -0700

    Added Login and Registration
    _New: login and registration fragments and respective layout xml files.
    _Tested functionality for both local and remote success and errors with the evidence of monitoring the Heroku app log.
    _Changed MainActivity to now load the login fragment upon first starting the app.
    _Created Database instance on Heroku, and created tables.
    _

---
+++ app/src/main/java/group1/tcss450/uw/edu/messageappgroup1/SuccessFragment.java
@@ -0,0 +1,112 @@
+package group1.tcss450.uw.edu.messageappgroup1;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * Use the {@link SuccessFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class SuccessFragment extends Fragment implements View.OnClickListener {
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    //private OnFragmentInteractionListener mListener;
+
+    public SuccessFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment SuccessFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static SuccessFragment newInstance(String param1, String param2) {
+        SuccessFragment fragment = new SuccessFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_success, container, false);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        /*if (getArguments() == null) {
+            Log.wtf("", "getArguments() is null.");
+        } else {
+            String s = getArguments().getString(getString(R.string.keyUsername));
+            updateContent(s);
+        }*/
+    }
+
+    // TODO: Rename method, update argument and hook method into UI event
+    /*public void onButtonPressed(Uri uri) {
+        if (mListener != null) {
+            mListener.onFragmentInteraction(uri);
+        }
+    }*/
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        /*if (context instanceof OnFragmentInteractionListener) {
+            mListener = (OnFragmentInteractionListener) context;
+        } else {
+            throw new RuntimeException(context.toString()
+                    + " must implement OnFragmentInteractionListener");
+        }*/
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        //mListener = null;
+    }
+
+    @Override
+    public void onClick(View v) {
+
+    }
+
+    /*public void updateContent(final String username) {
+        TextView tv = getActivity().findViewById(R.id.textView_display);
+        tv.setText(username);
+    }*/
+
+}
